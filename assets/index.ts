38861eda54db639e210552d4686dafecf0613466
madhavarora10 <madhavarora9654@gmail.com>
2024-02-25 21:44:53 +0530

    first

---
+++ assets/index.ts
@@ -0,0 +1,5 @@
+const index = [{
+  heading: 'hello',
+}];
+
+export { index };
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/function-component-definition */
+export default function Page() {
+  console.log('hello');
+  return (
+    <div className=" ">
+      <h1 className="">hello</h1>
+    </div>
+  );
+}
